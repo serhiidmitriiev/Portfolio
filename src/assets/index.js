79c2cccd822ebc190d47afa8ea25d85b2eeffe32
Serhii Dmitriiev <serhii.dmitriiev@paymentology.com>
2024-02-27 18:52:31 +0400

    added new experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,12 +7,14 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import jest from "./tech/jest.png";
+import playwright from "./tech/playwright.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
+import nextjs from "./tech/nextjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import material from "./tech/material.png";
@@ -20,6 +22,7 @@ import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
 import andersenLab from "./company/andersenLab.png";
+import nerdySoft from "./company/nerdySoft.png";
 
 import nasa from "./nasa.jpg";
 import portfolio from "./portfolio.jpg";
@@ -33,6 +36,7 @@ export {
   close,
   css,
   jest,
+  playwright,
   material,
   git,
   html,
@@ -40,11 +44,13 @@ export {
   mongodb,
   nodejs,
   reactjs,
+  nextjs,
   redux,
   tailwind,
   typescript,
   threejs,
   andersenLab,
+  nerdySoft,
   nasa,
   portfolio,
 };
